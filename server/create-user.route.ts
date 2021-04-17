6e5054dda3481e8b3c7882dc215a177020298a50
IrinaBBB <balkarovairina@gmail.com>
2021-04-17 11:18:31 +0200

    add: initial

--- server/create-user.route.ts
+++ server/create-user.route.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 import { db } from './database';
 import { validatePassword } from './password-validation';
-import { randomBytes } from './security.utils';
+import { createSessionToken, randomBytes } from './security.utils';
 import { sessionStore } from './session-store';
 
 const argon2 = require('argon2');
@@ -21,19 +21,11 @@ async function createUserAndSession(res: Response, credentials): Promise<any> {
     const passwordDigest = await argon2.hash(credentials.password);
     const user = db.createUser(credentials.email, passwordDigest);
 
-    const sessionId = await randomBytes(32).then((bytes) =>
-        bytes.toString('hex')
-    );
+    const sessionToken = await createSessionToken(user.id.toString());
 
-    console.log('sessionId: ' + sessionId);
-
-    sessionStore.createSession(sessionId, user);
-
-    res.cookie('SESSION_ID', sessionId, {
+    res.cookie('SESSION_ID', sessionToken, {
         httpOnly: true,
         secure: true,
     });
-    res.header('Access-Control-Allow-Credentials');
     res.status(200).json({ id: user.id, email: user.email });
-    console.log('User created');
 }
--- server/security.utils.ts
+++ server/security.utils.ts
@@ -1,4 +1,20 @@
+import * as fs from 'fs';
+import * as jwt from 'jsonwebtoken';
+
 const util = require('util');
 const cryptoUtil = require('crypto');
 
 export const randomBytes = util.promisify(cryptoUtil.randomBytes);
+export const signJwt = util.promisify(cryptoUtil.randomBytes);
+
+const RSA_PRIVATE_KEY = fs.readFileSync('./demos/private.key');
+const RSA_PUBLIC_KEY = fs.readFileSync('./demos/public.key');
+const SESSION_DURATION = 240;
+
+export async function createSessionToken(userId: string): Promise<string> {
+    return jwt.sign({}, RSA_PRIVATE_KEY, {
+        algorithm: 'RS256',
+        expiresIn: SESSION_DURATION,
+        subject: userId,
+    });
+}
